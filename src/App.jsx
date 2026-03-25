aba7b5e70f69ce5ae2ca4f279b6f5d4e07421c29
OliverSM95 <Olisimm2006@gmail.com>
2026-03-25 01:46:35 -0400

    switch to react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '40px', textAlign: 'center' }}>
+      <h1>OliverSM95</h1>
+      <p>Full Stack Developer | JavaScript Enthusiast</p>
+      <hr />
+      <div style={{ marginTop: '20px' }}>
+        <h2>My Projects</h2>
+        <p>Project 1: Personal Portfolio (You are looking at it!)</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
